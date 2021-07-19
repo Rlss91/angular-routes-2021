a03096f02a66730691cbd472a2624352531c3765
rlss91 <rlss91@gmail.com>
2021-07-19 11:11:57 +0300

    1

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-page1',
   templateUrl: './page1.component.html',
-  styleUrls: ['./page1.component.css']
+  styleUrls: ['./page1.component.css'],
 })
 export class Page1Component implements OnInit {
-
-  constructor() { }
+  qdata: any;
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.qdata = {
+      data: this.route.snapshot.queryParams.data,
+    };
+    console.log('queryParams', this.route.snapshot.queryParams);
+    this.route.queryParams.subscribe((ob) => {
+      console.log('ob', ob);
+    });
   }
 
+  navigateTo(): void {
+    // this.router.navigate(['home'], { relativeTo: this.route });
+    this.router.navigate(['home']);
+  }
 }
